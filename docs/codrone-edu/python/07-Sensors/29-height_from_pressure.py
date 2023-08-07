94c072315dfc112ca61f341da13fefb9a932d89d
harshN26 <hnihalani26@gmail.com>
2023-08-07 14:02:29 -0400

    fixed mm and cm swithc in height_from_pressure in py

--- docs/codrone-edu/python/07-Sensors/29-height_from_pressure.py
+++ docs/codrone-edu/python/07-Sensors/29-height_from_pressure.py
@@ -9,5 +9,5 @@ drone.set_initial_pressure()
 
 # The drone will not take off in this example but by moving it up and down manually the resulting height will print
 for i in range(300):
-    print(drone.height_from_pressure(), " millimeters")
+    print(drone.height_from_pressure(), " centimeters")
     time.sleep(0.2)
